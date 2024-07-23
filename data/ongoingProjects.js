1b15e1bb79646ee1b83baae0c5c0716b4b3a1a8b
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-07-23 16:17:30 +0530

    feat: Add IFrameComponent for displaying project location map

--- data/ongoingProjects.js
+++ data/ongoingProjects.js
@@ -4,7 +4,8 @@ export const projects = [
     title: "Project 1",
     image: "/images/projects/featuredProject1.jpg",
     description: "Short description of Project 1",
-    fullDescription: "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
+    fullDescription:
+      "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
     reraNumber: "P52000011751",
     amenitiesDescription: "Description of amenities for Project 1",
     amenities: [
@@ -18,24 +19,30 @@ export const projects = [
     connectivity: [
       " Panvel is a major transportation hub in Maharashtra, India, connecting various modes of transport such as railways, highways, and air travel. It serves as a junction for both local and long-distance travel",
       "It is well-connected by major highways like the Mumbai-Pune Expressway (NH 48) and Sion-Panvel Expressway (NH 4B), facilitating smooth road transport to Mumbai, Pune, and other cities in Maharashtra.",
-      " Panvel's strategic location places it near Mumbai's Chhatrapati Shivaji Maharaj International Airport and the upcoming Navi Mumbai International Airport, enhancing air connectivity for residents and businesses."
+      " Panvel's strategic location places it near Mumbai's Chhatrapati Shivaji Maharaj International Airport and the upcoming Navi Mumbai International Airport, enhancing air connectivity for residents and businesses.",
     ],
     map: {
       center: { lat: 19.009816908187975, lng: 73.10259316005418 }, // Coordinates for Crown Housing
       zoom: 14,
       markers: [
-        { lat: 19.009816908187975, lng: 73.10259316005418, title: "Project 1 Location" },
+        {
+          lat: 19.009816908187975,
+          lng: 73.10259316005418,
+          title: "Project 1 Location",
+        },
         // You can add additional markers if needed
       ],
-      iframeSrc: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d695.8197463799711!2d73.10259316005418!3d19.009816908187975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7e911b8186ff7%3A0x8637ac85995699fd!2sCrown%20Housing!5e1!3m2!1sen!2sin!4v1721728374695!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
-    }
-  },  
+      iframeSrc:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d695.8197463799711!2d73.10259316005418!3d19.009816908187975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7e911b8186ff7%3A0x8637ac85995699fd!2sCrown%20Housing!5e1!3m2!1sen!2sin!4v1721728374695!5m2!1sen!2sin",
+    },
+  },
   {
     slug: "project-2",
     title: "Project 2",
     image: "/images/projects/featuredProject1.jpg",
     description: "Short description of Project 2",
-    fullDescription: "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
+    fullDescription:
+      "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
     reraNumber: "P52000011751",
     amenitiesDescription: "Description of amenities for Project 1",
     amenities: [
@@ -46,24 +53,25 @@ export const projects = [
       { name: "Gym", image: "/images/amenities/gym.jpg" },
       { name: "Lift", image: "/images/amenities/lift.jpg" },
     ],
-    connectivity: [
-      "Situated Just 10 Minutes away from Neral Railway Station",
-    ],
+    connectivity: ["Situated Just 10 Minutes away from Neral Railway Station"],
     map: {
-      center: { lat: 19.0330, lng: 73.3145 }, // Example coordinates for Neral
+      center: { lat: 19.033, lng: 73.3145 }, // Example coordinates for Neral
       zoom: 14,
       markers: [
-        { lat: 19.0330, lng: 73.3145, title: "Project 1 Location" },
-        { lat: 19.0280, lng: 73.3110, title: "Neral Railway Station" },
-      ]
-    }
+        { lat: 19.033, lng: 73.3145, title: "Project 1 Location" },
+        { lat: 19.028, lng: 73.311, title: "Neral Railway Station" },
+      ],
+      iframeSrc:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d695.8197463799711!2d73.10259316005418!3d19.009816908187975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7e911b8186ff7%3A0x8637ac85995699fd!2sCrown%20Housing!5e1!3m2!1sen!2sin!4v1721728374695!5m2!1sen!2sin",
+    },
   },
   {
     slug: "project-3",
     title: "Project 3",
     image: "/images/projects/featuredProject1.jpg",
     description: "Short description of Project 3",
-    fullDescription: "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
+    fullDescription:
+      "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
     reraNumber: "P52000011751",
     amenitiesDescription: "Description of amenities for Project 1",
     amenities: [
@@ -74,17 +82,15 @@ export const projects = [
       { name: "Gym", image: "/images/amenities/gym.jpg" },
       { name: "Lift", image: "/images/amenities/lift.jpg" },
     ],
-    connectivity: [
-      "Situated Just 10 Minutes away from Neral Railway Station",
-    ],
+    connectivity: ["Situated Just 10 Minutes away from Neral Railway Station"],
     map: {
-      center: { lat: 19.0330, lng: 73.3145 }, // Example coordinates for Neral
+      center: { lat: 19.033, lng: 73.3145 }, // Example coordinates for Neral
       zoom: 14,
       markers: [
-        { lat: 19.0330, lng: 73.3145, title: "Project 1 Location" },
-        { lat: 19.0280, lng: 73.3110, title: "Neral Railway Station" },
-      ]
-    }
+        { lat: 19.033, lng: 73.3145, title: "Project 1 Location" },
+        { lat: 19.028, lng: 73.311, title: "Neral Railway Station" },
+      ],
+    },
   },
 ];
 
