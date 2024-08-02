eec23b9a1616c600e24bdcc8b59e0328ba785838
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-02 18:03:29 +0530

    new code

--- app/projects/ongoingProjects/[slug]/page.jsx
+++ app/projects/ongoingProjects/[slug]/page.jsx
@@ -86,6 +86,8 @@ export default function ProjectPage({ params }) {
               </div>
             </section>
           )}
+
+          
           <div className="bg-gray-100 p-4">
             <div className="max-w-6xl mx-auto">
               <h1 className="text-3xl font-bold text-center mb-8">Configuration</h1>
