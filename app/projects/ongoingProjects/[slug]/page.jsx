de60e9dfc3924ce7bcca606a028f15e17fa0c90f
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-09 15:24:31 +0530

    changes in projects section

--- app/projects/ongoingProjects/[slug]/page.jsx
+++ app/projects/ongoingProjects/[slug]/page.jsx
@@ -22,14 +22,14 @@ export default function ProjectPage({ params }) {
       <div className="w-full max-w-7xl px-4 sm:px-6 lg:px-8">
         <section className='bg-white py-8'>
           <div className='flex flex-col lg:flex-row items-start'>
-            <div className='lg:w-1/2 lg:pr-6'>
-              <h1 className='text-2xl md:text-3xl font-bold mb-4 text-black text-center lg:text-left'>
+            <div className='lg:w-1/2 lg:pr-6 text-center'>
+              <h1 className='text-2xl md:text-3xl font-bold mb-4 text-black text-center lg:text-center'>
                 {project.title}
               </h1>
               <p className='mb-4 text-sm md:text-base text-black'>
                 {project.fullDescription}
               </p>
-              <p className='mb-4 text-sm md:text-base font-semibold text-black'>
+              <p className='mb-4 text-sm md:text-base font-semibold text-black text-center'>
                 Maha Rera Registration No : {project.reraNumber || "N/A"}
               </p>
             </div>
@@ -45,7 +45,7 @@ export default function ProjectPage({ params }) {
           </div>
           <div className="mt-4 flex justify-center lg:justify-start">
             <Link href={project.pdfLink} target="_blank" rel="noopener noreferrer">
-              <button className="bg-red-600 hover:bg-red-700 text-white font-bold text-sm sm:text-base py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out flex items-center justify-center">
+              <button className="bg-red-600 hover:bg-red-700 text-white  font-bold text-sm sm:text-base py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out flex items-center justify-center">
                 Download Brochure
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5 ml-1 sm:ml-2" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
@@ -56,7 +56,7 @@ export default function ProjectPage({ params }) {
         </section>
 
         {project.amenities && (
-          <section className='bg-gray-100 py-8'>
+          <section className='bg-gray-200 py-8'>
             <h2 className='text-xl md:text-2xl font-bold mb-4 text-black text-center'>Amenities</h2>
             <p className='mb-4 text-sm md:text-base text-black text-center'>
               {project.amenitiesDescription}
@@ -108,24 +108,25 @@ export default function ProjectPage({ params }) {
               </tbody>
             </table>
           </div>
+          <div className="bg-gray-200">
+            <h2 className="text-3xl font-bold text-center mb-8">Floor Plans</h2>
 
-          <h2 className="text-3xl font-bold text-center mb-8">Floor Plans</h2>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {project.floorplans.map((plan, index) => (
-              <div key={index}>
-                <div className="relative w-full h-64">
-                  <Image
-                    src={plan.src}
-                    alt={plan.title}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-lg"
-                  />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {project.floorplans.map((plan, index) => (
+                <div key={index}>
+                  <div className="relative w-full h-64">
+                    <Image
+                      src={plan.src}
+                      alt={plan.title}
+                      layout="fill"
+                      objectFit="cover"
+                      className="rounded-lg"
+                    />
+                  </div>
+                  <p className="text-center font-semibold mt-4">{plan.title}</p>
                 </div>
-                <p className="text-center font-semibold mt-4">{plan.title}</p>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </section>
 
