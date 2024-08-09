22abf6f853d04e1a730f717a6be88b58ccb01d2d
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-09 13:26:58 +0530

    project/ongoingprojects

--- app/projects/ongoingProjects/[slug]/page.jsx
+++ app/projects/ongoingProjects/[slug]/page.jsx
@@ -1,175 +1,81 @@
-import IFrameComponent from "@/components/IFrameComponent";
-import { projects, getProjectBySlug } from "../../../../data/ongoingProjects";
-import Image from "next/image";
-import Link from "next/link";
-import Calculator from "@/components/Calculator";
+'use client'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { titleVariants } from '../../../../utils/animation';
+import { projects } from '../../../../data/ongoingProjects';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export async function generateStaticParams() {
-  return projects.map((project) => ({
-    slug: project.slug,
-  }));
-}
+const ProjectCard = ({ project }) => (
+  <motion.div
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true, amount: 0.1 }}
+    variants={{
+      visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+      hidden: { opacity: 0, x: 100 }
+    }}
+    whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+    className="bg-gray-100 rounded-lg overflow-hidden shadow-lg flex flex-col h-full border-2 border-red-800"
+  >
+    <div className="relative h-48 sm:h-56 md:h-64">
+      <Image
+        src={project.image}
+        layout="fill"
+        objectFit="cover"
+        alt={project.title}
+      />
+    </div>
+    <div className="p-4 flex-grow flex flex-col justify-between">
+      <div>
+        <h3 className="text-lg sm:text-xl font-semibold mb-2">{project.title}</h3>
+        <p className="text-gray-600 text-sm mb-4">{project.description}</p>
+      </div>
+      <Link href={`/projects/ongoingProjects/${project.slug}`}>
+        <span className="mt-auto w-full py-2 px-4 bg-gradient-to-r from-red-600 to-red-800 text-white rounded-md hover:from-red-700 hover:to-red-900 transition duration-300 text-sm sm:text-base inline-block text-center">
+          Read More
+        </span>
+      </Link>
+    </div>
+  </motion.div>
+);
 
-export default function ProjectPage({ params }) {
-  const project = getProjectBySlug(params.slug);
-
-  if (!project) {
-    return <div>Project not found</div>;
-  }
-
+const OngoingProjects = () => {
   return (
-    <div className="min-h-screen">
-      <section className='bg-white py-8'>
-        <div className='flex flex-col lg:flex-row items-start px-4'>
-          <div className='lg:w-3/3 pr-6'>
-            <h1 className='text-2xl md:text-3xl font-bold mb-4 text-black text-center'>
-              {project.title}
-            </h1>
-            <p className='mb-4 text-sm md:text-base text-black'>
-              {project.fullDescription}
-            </p>
-            <p className='mb-4 text-sm md:text-base font-semibold text-black'>
-              Maha Rera Registration No : {project.reraNumber || "N/A"}
-            </p>
-          </div>
-          <div className='lg:w-2/3'>
-            <Image
-              src={project.image}
-              alt={project.title}
-              width={600}
-              height={400}
-              className='w-full h-auto object-cover'
-            />
-          </div>
-        </div>
-        <div className="px-8 mt-4 flex gap-2">
-          <Link href={project.pdfLink} target="_blank" rel="noopener noreferrer">
-            <button className="bg-red-600 hover:bg-red-700 text-white font-bold text-sm sm:text-base py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out flex items-center justify-center w-full sm:w-auto">
-              Download Brochure
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5 ml-1 sm:ml-2" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-              </svg>
-            </button>
-          </Link>
-          {/* <Link href="/Calculator">
-            <button className="bg-red-600 hover:bg-red-700 text-white font-bold text-sm sm:text-base py-2 px-3 sm:px-4 rounded transition duration-300 ease-in-out flex items-center justify-center w-full sm:w-auto">
-              EMI Calculator
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5 ml-1 sm:ml-2" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-              </svg>
-            </button>
-          </Link> */}
+    <div>
+      <div className="bg-[url('/images/projectBackground.jpg')] bg-center bg-cover h-[300px] sm:h-[400px] md:h-[500px] lg:h-[500px]">
+        <motion.h1
+          initial='offscreen'
+          whileInView={"onscreen"}
+          variants={titleVariants}
+          className='container py-32 text-6xl font-semibold text-white tracking-widest text-center lg:py-32'
+        >
+          Ongoing Projects
+        </motion.h1>
+      </div>
 
-        </div>
-      </section>
-
-      {project.amenities && (
-        <section className='bg-gray-100 py-8'>
-          <div className='px-4'>
-            <h2 className='text-xl md:text-2xl font-bold mb-4 text-black text-center'>Amenities</h2>
-            <p className='mb-4 text-sm md:text-base text-black text-center'>
-              {project.amenitiesDescription}
-            </p>
-            <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4'>
-              {project.amenities.map((amenity, index) => (
-                <div key={index} className='relative'>
-                  <Image
-                    src={amenity.image}
-                    alt={amenity.name}
-                    width={200}
-                    height={200}
-                    className='rounded-lg w-full h-64'
-                  />
-                  <div className='absolute bottom-0 left-0 right-0 bg-blue-500 bg-opacity-75 text-white p-2 rounded-b-lg'>
-                    <p className='text-center text-xs sm:text-sm'>
-                      {amenity.name}
-                    </p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </section>
-      )}
-
-      <section className='bg-white py-8'>
-        <div className='px-4'>
-          <h1 className="text-3xl font-bold text-center mb-8">Configuration</h1>
-
-          <div className="overflow-x-auto mb-12">
-            <table className="w-full border-collapse bg-white shadow-md rounded-lg border border-gray-300">
-              <thead>
-                <tr className="bg-navy-700 text-white border-b border-gray-300 transition duration-300 ease-in-out hover:bg-navy-800">
-                  <th className="py-3 px-4 text-left border-r border-gray-300">Configuration</th>
-                  <th className="py-3 px-4 text-left border-r border-gray-300">SBA / Carpet (sq.ft.)</th>
-                  <th className="py-3 px-4 text-left">Price</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="transition duration-300 ease-in-out hover:bg-gray-100 border-b border-gray-300">
-                  <td className="py-3 px-4 border-r border-gray-300">1 BHK</td>
-                  <td className="py-3 px-4 border-r border-gray-300">396-425 sq.ft</td>
-                  <td className="py-3 px-4">
-                    <button className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-1 px-3 rounded transition duration-300 ease-in-out">
-                      Get Quote
-                    </button>
-                  </td>
-                </tr>
-                {/* Add more rows here as needed */}
-              </tbody>
-            </table>
+      <div className='container mx-auto px-4'>
+        <div className='lg:py-20'>
+          <div className='pt-8 pb-4'>
+            <motion.h1
+              initial='offscreen'
+              whileInView={"onscreen"}
+              variants={titleVariants}
+              className='text-4xl font-bold tracking-wider text-center uppercase'
+            >
+              Our Current Projects
+            </motion.h1>
           </div>
 
-
-          <h2 className="text-3xl font-bold text-center mb-8">Floor Plans</h2>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {project.floorplans.map((plan, index) => (
-              <div key={index}>
-                <div className="relative w-full h-64">
-                  <Image
-                    src={plan.src}
-                    alt={plan.title}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-lg"
-                  />
-                </div>
-                <p className="text-center font-semibold mt-4">{plan.title}</p>
-              </div>
+          <div className='grid py-8 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+            {projects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
             ))}
           </div>
         </div>
-      </section>
-
-      <div className='grid grid-cols-1 md:grid-cols-2'>
-        {project.connectivity && (
-          <section className='bg-gray-100 py-8'>
-            <div className='px-4'>
-              <h2 className='text-xl md:text-2xl font-bold mb-4 text-black text-center'>
-                Connected To Everything
-              </h2>
-              <ul className='list-disc pl-5 mb-4 text-sm md:text-base text-black'>
-                {project.connectivity.map((item, index) => (
-                  <li key={index} className='mb-2'>
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-        )}
-        {project.map && (
-          <section className='bg-gray-100 py-8'>
-            <div className='px-4'>
-              <h2 className='text-xl md:text-2xl font-bold mb-4 text-black text-center'>Project Location</h2>
-              <div className='w-full h-[300px] md:h-[450px] rounded-lg overflow-hidden'>
-                <IFrameComponent iframeSrc={project.map.iframeSrc} />
-              </div>
-            </div>
-          </section>
-        )}
       </div>
     </div>
   );
-}
+};
+
+export default OngoingProjects;
