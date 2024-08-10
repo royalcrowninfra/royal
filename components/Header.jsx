99ff5cc7415cdc8b26162fb7e39f7cfe41090d58
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-10 12:07:49 +0530

    mobile nav updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -74,13 +74,13 @@ const Navbar = () => {
             width={140}
             height={60}
             alt="Royal Crown Realty"
-            className="h-20 w-auto sm:h-20 md:h-24 lg:h-32 transition-all duration-300"
+            className="h-30 w-30 sm:h-20 md:h-24 lg:h-32 transition-all duration-300"
           />
         </Link>
       </div>
       {/* Top bar */}
       <div className="bg-gradient-to-r bg-cyan-700 py-2">
-        <div className="container mx-auto px-8 flex flex-col sm:flex-row justify-end items-center gap-2 sm:gap-12">
+        <div className="container mx-auto px-12 flex flex-col sm:flex-row justify-end items-end  gap-0 sm:gap-12">
           <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-6 mb-2 sm:mb-0">
             <span className="text-white text-xs sm:text-sm flex items-center">
               <Phone size={16} className="mr-2" />
@@ -109,11 +109,17 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+        <div className="sm:hidden absolute top-4 right-4 z-50">
+          <button onClick={toggleMobileMenu} className="text-black-500 hover:text-gray-600">
+            {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+        </div>
+
       </div>
 
       {/* Main navigation */}
 
-      <div className="flex items-start justify-center ml-96 py-4 sm:ml-0 sm:py-8 bg-gray-100 border-b-1 border-black">
+      <div className="flex items-start justify-center ml-96 py-0 sm:ml-0 sm:py-8 bg-gray-100 border-b-1 border-black">
         {/* Desktop Navigation Links */}
         <div className="hidden sm:flex items-center  space-x-6 sm:ml-64 lg:gap-8">
           {links.map((item, index) => (
@@ -149,11 +155,11 @@ const Navbar = () => {
       </div>
 
       {/* Mobile menu button */}
-      <div className="sm:hidden fixed top-4 right-4 z-50">
-        <button onClick={toggleMobileMenu} className="text-gray-500 hover:text-gray-600">
+      {/* <div className="sm:hidden fixed top-4 right-4 z-50">
+        <button onClick={toggleMobileMenu} className="text-black-500 hover:text-gray-600">
           {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
-      </div>
+      </div> */}
 
       {/* Mobile Navigation Links */}
       <div
