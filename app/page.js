0d6c479b006df71bd3b00710da77876d466430cd
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-10 17:04:16 +0530

    new code

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import AboutComponent from "../components/AboutComponent";
 import BankingPartner from "../components/BankingPartner";
 import CatalogSwiperSection from "../components/CatalogSwiperSection";
-// import CompanySection from "../components/CompanySection";
 import ContactSection from "../components/ContactSection";
 import HeroSection from "../components/HeroSection";
 
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <CompanySection /> */}
       <AboutComponent />
       <CatalogSwiperSection />
       <ContactSection />
