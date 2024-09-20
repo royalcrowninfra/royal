a1f2e87fd12fa23b9305c01e07d976245f4b7911
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-20 15:24:15 +0530

    connect to everything update

--- data/ongoingProjects.js
+++ data/ongoingProjects.js
@@ -24,7 +24,7 @@ export const projects = [
     ],
     connectivity: [
       "PANVEL RAILWAY STATION : 3.5 KMS",
-      "It is well-connected by major highways like the Mumbai-Pune Expressway (NH 48) and Sion-Panvel Expressway (NH 4B), facilitating smooth road transport to Mumbai, Pune, and other cities in Maharashtra.",
+      "KHANDESHWAR RAILWAY STATION : 3.5 KMS",
       "Panvel's strategic location places it near Mumbai's Chhatrapati Shivaji Maharaj International Airport and the upcoming Navi Mumbai International Airport, enhancing air connectivity for residents and businesses.",
     ],
     map: {
