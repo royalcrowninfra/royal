dc012c3013d2c8b91c96bdcb40a892b470122337
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-21 12:27:11 +0530

    footer updated to new font

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ import { MapPin, Phone, Mail, Globe } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <div className="bg-gray-700 text-white">
+    <div className="bg-gray-700 text-white font-['Arial Rounded MT Bold', 'Arial', sans-serif]">
       <div className="container mx-auto px-4 py-14">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <motion.div
@@ -19,7 +19,7 @@ export default function Footer() {
             <h2 className="text-xl font-semibold uppercase md:mx-6">Corporate Office</h2>
             <div className="flex items-start space-x-2">
               <MapPin className="mt-1 flex-shrink-0" />
-              <p>ROYAL CROWN INFRA<br />Shop No 1 Sector 25 Plot No 19,<br />
+              <p className="text-sm sm:text-base">ROYAL CROWN INFRA<br />Shop No 1 Sector 25 Plot No 19,<br />
                 Ravi Rachna, near Khandeshwar Station,<br />
                 Sector-25, Khandeshwar,<br />
                 Navi Mumbai, Maharashtra 410209</p>
@@ -36,19 +36,19 @@ export default function Footer() {
             <div className="space-y-2 ml-2">
               <div className="flex items-center space-x-2">
                 <Phone size={18} />
-                <p>CALL US :  (+91) 8689992777</p>
+                <p className="text-sm sm:text-base">CALL US :  (+91) 8689992777</p>
               </div>
               <div className="flex items-center space-x-2">
                 <Phone size={18} />
-                <p>WHATSAPP :  (+91) 8689992777</p>
+                <p className="text-sm sm:text-base">WHATSAPP :  (+91) 8689992777</p>
               </div>
               <div className="flex items-center space-x-2">
                 <Mail size={18} />
-                <p>EMAIL :   info@royalcrownreality.com</p>
+                <p className="text-sm sm:text-base">EMAIL :   info@royalcrownreality.com</p>
               </div>
               <div className="flex items-center space-x-2">
                 <Globe size={18} />
-                <p>WEBSITE : www.royalcrown.in</p>
+                <p className="text-sm sm:text-base">WEBSITE : www.royalcrown.in</p>
               </div>
             </div>
           </motion.div>
@@ -61,10 +61,10 @@ export default function Footer() {
           >
             <h2 className="text-xl font-semibold uppercase md:mx-16">Quick Links</h2>
             <div className="flex flex-col space-y-2 ml-2 md:mx-20">
-              <Link href='/about/ourteam' className="hover:underline">About Us</Link>
-              <Link href='/projects' className="hover:underline">Projects</Link>
-              <Link href='/gallery' className="hover:underline">Gallery</Link>
-              <Link href='/contact' className="hover:underline">Contact</Link>
+              <Link href='/about/ourteam' className="hover:underline text-sm sm:text-base">About Us</Link>
+              <Link href='/projects' className="hover:underline text-sm sm:text-base">Projects</Link>
+              <Link href='/gallery' className="hover:underline text-sm sm:text-base">Gallery</Link>
+              <Link href='/contact' className="hover:underline text-sm sm:text-base">Contact</Link>
             </div>
           </motion.div>
 
@@ -76,10 +76,10 @@ export default function Footer() {
           >
             <h2 className="text-xl font-semibold uppercase">Connect With Us</h2>
             <div className="flex flex-col space-y-2 ml-2 md:mx-12">
-              <Link href=' https://www.instagram.com/royalcrown.19/' className="hover:underline">Instagram</Link>
-              <Link href='/' className="hover:underline">Twitter</Link>
-              <Link href='/' className="hover:underline">LinkedIn</Link>
-              <Link href='https://www.facebook.com/royalcrowninfra/' className="hover:underline">Facebook</Link>
+              <Link href=' https://www.instagram.com/royalcrown.19/' className="hover:underline text-sm sm:text-base">Instagram</Link>
+              <Link href='/' className="hover:underline text-sm sm:text-base">Twitter</Link>
+              <Link href='/' className="hover:underline text-sm sm:text-base">LinkedIn</Link>
+              <Link href='https://www.facebook.com/royalcrowninfra/' className="hover:underline text-sm sm:text-base">Facebook</Link>
             </div>
           </motion.div>
         </div>
@@ -94,12 +94,11 @@ export default function Footer() {
       >
         <div className="container mx-auto px-4 text-center md:flex md:justify-between md:items-center">
           <div className="mb-4 md:mb-0">
-            <p>&copy; 2024 Royal Crown Infra. All Rights Reserved</p>
-            {/* <p className="text-sm text-gray-400">Designed By Akshay Kalamkar</p> */}
+            <p className="text-sm sm:text-base">&copy; 2024 Royal Crown Infra. All Rights Reserved</p>
           </div>
           <div className="space-x-4">
-            <Link className="hover:underline" href='/'>Privacy Policy</Link>
-            <Link className="hover:underline" href='/'>Terms of Service</Link>
+            <Link className="hover:underline text-sm sm:text-base" href='/'>Privacy Policy</Link>
+            <Link className="hover:underline text-sm sm:text-base" href='/'>Terms of Service</Link>
           </div>
         </div>
       </motion.div>
