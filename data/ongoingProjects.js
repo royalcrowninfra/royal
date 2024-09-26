c801ac8accc1815751c89f5ba2a13925bbe1118c
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-26 11:06:42 +0530

    floorplans

--- data/ongoingProjects.js
+++ data/ongoingProjects.js
@@ -19,9 +19,9 @@ export const projects = [
       { name: "Lift", image: "/images/amenities/lift.jpg" },
     ],
     floorplans: [
-      { title: '1 BHK Floor Plan', src: '/images/projects/ongoing/saiprasad/img-10.jpeg' },
-      { title: '2 BHK Floor Plan', src: '/images/projects/ongoing/saiprasad/img-11.jpeg' },
-      { title: 'Ground Floor Plan', src: '/images/projects/ongoing/saiprasad/img-9.jpeg' },
+      { title: '1 BHK Floor Plan', src: '/images/floorplans/ongoing/saiprasad/img-10.jpeg' },
+      { title: '2 BHK Floor Plan', src: '/images/floorplans/ongoing/saiprasad/img-11.jpeg' },
+      { title: 'Ground Floor Plan', src: '/images/floorplans/ongoing/saiprasad/img-9.jpeg' },
     ],
     connectivity: [
       "PANVEL RAILWAY STATION : 3.5 KMS",
